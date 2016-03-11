54ab39e723cb442cff9439ab9e49af0b25285af9
moonlight87 <coganglen1992@X>
2016-03-11 09:36:36 +0700

    v20160311

---
+++ subtype-skin/default/js/mysearch-items.js
@@ -0,0 +1,10 @@
+/**
+ * Created by User on 3/9/2016.
+ */
+$j(document).ready(function(){
+    $j(".mysearch-header").click(function(){
+        $j("#header-search").toggle();
+    });
+
+});
+
---
+++ subtype-skin/default/js/subtype.js
@@ -0,0 +1,6 @@
+/**
+ * Created by User on 3/1/2016.
+ */
+$j(document).ready(function(){
+
+});
